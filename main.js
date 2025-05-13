1bf13ccced9322b520b80e94202658ea9d440808
chitransh-usar <ggsipuusar2327@gmail.com>
2025-05-13 10:11:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", {
+  videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
+});
+ Shery.makeMagnet(".main-content h1");
+
+gsap.to(".fleftelem", {
+  scrollTrigger: {
+    trigger: "#fimages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 0.5,
+  },
+  y: "-300%",
+  ease: Power1,
+});
+
+let sections = document.querySelectorAll(".fleftelem");
+/*
+Shery.imageEffect(".images", {
+  style: 4,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    sections.forEach(function (section, index) {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
+*/
+
+Shery.imageEffect(".images", {
+  style: 5,
+  slideStyle: (setScroll) => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY / innerHeight); //Updating the scroll
+    });
+  },
+});
